61d31957f4792b697a617b5decd0cd6e466783da
AmirAhmedc <aaamer.aaahmed@gmail.com>
2024-06-22 14:53:41 +0300

    add and register components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="home"><h1>home page</h1></div>
-  <v-icon></v-icon>
+  <!-- <div class="home"><h1>home page</h1></div> -->
+  <!-- <v-icon></v-icon> -->
+  <UpperBanner />
+  <TheFeatures/>
+  <TopOffers />>
+  
 </template>
 
 <script>
+import UpperBanner from "@/components/home_page/UpperBanner";
+import TheFeatures from "@/components/home_page/TheFeatures";
+import TopOffers from "@/components/home_page/TopOffers"
 // @ is an alias to /src
-
 export default {
   name: "HomeView",
-  components: {},
+  components: { UpperBanner, TheFeatures , TopOffers },
 };
 </script>
